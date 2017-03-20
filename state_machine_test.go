dd460ac308056c8b7e0dacdc87b40f592aca872e
Martin Vium <martin.vium@gmail.com>
2017-03-20 23:33:48 +0100

    Fix state machine test

--- state_machine_test.go
+++ state_machine_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestStateMachine_String(t *testing.T) {
-	s := NewStateMachine(nil)
+	s := NewStateMachine()
 	if s.String() != "unstarted" {
 		t.Fail()
 	}
@@ -12,7 +12,7 @@ func TestStateMachine_String(t *testing.T) {
 func TestStateMachine_TransitionSuccess(t *testing.T) {
 	t.Skip("Currently need a whole game server to run this, need to find a way to extract this")
 
-	s := NewStateMachine(nil)
+	s := NewStateMachine()
 	s.Transition("mulligan")
 	if s.String() != "mulligan" {
 		t.Fail()
@@ -20,7 +20,7 @@ func TestStateMachine_TransitionSuccess(t *testing.T) {
 }
 
 func TestStateMachine_TransitionFail(t *testing.T) {
-	s := NewStateMachine(nil)
+	s := NewStateMachine()
 	s.Transition("main")
 	if s.String() != "unstarted" {
 		t.Fail()
